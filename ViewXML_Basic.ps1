ef4cd90e718a9ff07916ab29666cfca57d222b65
way0utwest <sjones@dkranch.net>
2015-01-02 11:04:01 -0700

    Initial Commit
    Basic commit to download a file from the internet for ViewXML and save
    it in the GETXML

---
+++ ViewXML_Basic.ps1
@@ -0,0 +1,23 @@
+﻿#ViewXML_Basic
+# View XML file data from a website
+
+$debug = 1;
+# counter for events
+$i = 1
+$baseURL = "http://www.sqlsaturday.com/eventxml.aspx?sat="
+
+$sourceURL = $baseURL + $i
+
+# debug information
+if ($debug -eq 2) {
+  Write-Host $sourceURL
+  }
+
+$doc = New-Object System.Xml.XmlDocument
+$doc.Load($sourceURL)
+
+$nodes = $doc.SelectNodes("//[@event]")
+foreach ($element in $nodes) {
+  Write-Host "Speaker:" + $element.attributes["title"].value
+  }
+
---
+++ getxml.ps1
@@ -0,0 +1,24 @@
+﻿# get SQL Saturday data from the site
+
+$debug = 1;
+# counter for events
+$i = 1
+$baseURL = "http://www.sqlsaturday.com/eventxml.aspx?sat="
+
+
+$DestinationFile = "E:\SQLSatData\SQLSat" + $i + ".xml"
+$sourceURL = $baseURL + $i
+
+# debug information
+if ($debug -eq 1) {
+  write-host $DestinationFile
+  }
+
+if ($debug -eq 2) {
+  Write-Host $sourceURL
+  }
+
+
+# Get file from web server
+Invoke-WebRequest $sourceURL -OutFile $DestinationFile
+
